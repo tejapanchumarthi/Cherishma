380f52caeec35a6e9d4d0d55296672074866753b
Teja <tejapanchumarthi@gmail.com>
2019-05-14 12:14:30 +0530

    Woah! The messag has been synced.

---
+++ RiskContent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+        }
+    }
+}
